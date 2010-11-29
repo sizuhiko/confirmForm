9b4ceda362249e788e50c5342578683e59b55eb3
Kenichiro Kishida <kishida@Kenichiro-Kishida-no-MacBook-Air.local>
2010-11-29 17:27:19 +0900

    Escape fix, and create min file.

--- src/confirmform.js
+++ src/confirmform.js
@@ -129,7 +129,7 @@
 				return;	// not checked
 			}
 			if(prev.name == this.name) {
-				td = td.append(options.separator+__formatValue(this, options));
+				td = td.append(jQuery.fn.text(options.separator+__formatValue(this, options)));
 			} else {
 				td = $('<td />').text(__formatValue(this, options));
 				table.append($('<tr />')
